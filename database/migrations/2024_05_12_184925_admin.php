9193ded8201402bafac71829b5e37fe0e79a824e
loli hunter <mh1225az@gmai.com>
2024-05-13 03:27:47 +0800

    second

--- database/migrations/2024_05_12_184925_admin.php
+++ database/migrations/2024_05_12_184925_admin.php
@@ -17,7 +17,6 @@ return new class extends Migration
            $table->string('keterangan');
            $table->unsignedBigInteger('user_id');
            $table->foreign('user_id')->references('id')->on('user');
-
        });
     }
 
